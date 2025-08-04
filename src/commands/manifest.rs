0014230cb3765607e93b49ebcc10e9ccb894bcf1
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-08-04 18:24:01 +0530

    Added Manifest file

---
+++ src/commands/manifest.rs
@@ -0,0 +1,10 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct RustyManifest {
+    pub name: String,
+    pub version: String,
+    pub author: String,
+    pub description: String,
+}
+
