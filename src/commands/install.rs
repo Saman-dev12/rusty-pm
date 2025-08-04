92c48274bc76556846d635359094bc951e6f01af
samanpreets84 <132365157+Saman-dev12@users.noreply.github.com>
2025-08-05 01:33:16 +0530

    Added install.rs

---
+++ src/commands/install.rs
@@ -0,0 +1,82 @@
+use flate2::read::GzDecoder;
+use reqwest::blocking::get;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::env;
+use std::fs::{self, create_dir_all, File};
+use std::io::{self, Cursor, Write};
+use std::path::{Path, PathBuf};
+use tar::Archive;
+
+use super::manifest::RustyManifest;
+
+
+pub fn install(package: &str) {
+    let current_dir = env::current_dir().expect("Failed to get current dir");
+    let rusty_json_path = current_dir.join("rusty.json");
+
+    if !rusty_json_path.exists() {
+        println!("❌ rusty.json not found. Run `rusty-pm init` first.");
+        return;
+    }
+
+    let rusty_file_data = File::open(&rusty_json_path).expect("Failed to open rusty.json");
+    let mut manifest: RustyManifest =
+        serde_json::from_reader(rusty_file_data).expect("Failed to parse rusty.json");
+
+    let node_modules = current_dir.join("node_moduless");
+    if !node_modules.exists() {
+        create_dir_all(&node_modules).expect("Failed to create node_modules folder");
+    }
+
+    let url = format!("https://registry.npmjs.org/{}", package);
+    let response = get(&url)
+        .expect("Failed to fetch package info")
+        .text()
+        .expect("Failed to read response");
+
+    let json: Value = serde_json::from_str(&response).expect("Invalid JSON");
+
+    let latest = json["dist-tags"]["latest"]
+        .as_str()
+        .expect("Latest version not found");
+
+    let tarball_url = json["versions"][latest]["dist"]["tarball"]
+        .as_str()
+        .expect("Tarball URL not found");
+
+    println!("📥 Downloading {}@{}...", package, latest);
+
+    let tarball = get(tarball_url)
+        .expect("Failed to download tarball")
+        .bytes()
+        .expect("Failed to get tarball bytes");
+
+    let tar = GzDecoder::new(Cursor::new(tarball));
+    let mut archive = Archive::new(tar);
+
+    let target_dir = node_modules.join(package);
+    create_dir_all(&target_dir).expect("Failed to create package folder");
+
+    archive
+        .unpack(&target_dir)
+        .expect("Failed to extract package");
+
+    println!(
+        "✅ Installed {}@{} to {}",
+        package,
+        latest,
+        target_dir.to_str().unwrap()
+    );
+
+    // Update description
+    manifest.description = format!(
+        "{}\nInstalled: {}@{}",
+        manifest.description, package, latest
+    );
+
+    let manifest_file = File::create(&rusty_json_path).expect("Failed to rewrite rusty.json");
+    serde_json::to_writer_pretty(manifest_file, &manifest)
+        .expect("Failed to write updated manifest");
+}
+
